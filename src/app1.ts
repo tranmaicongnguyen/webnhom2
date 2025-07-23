0996400978243266472c4b30321c6d11ed8cb0b9
tranmaicongnguyen <nguyentmcps28484@fpt.edu.vn>
2025-07-23 21:51:23 +0700

    Add initial App component with ExampleComponent integration

---
+++ src/app1.ts
@@ -0,0 +1,13 @@
+import React from 'react';
+import ExampleComponent from './components/ExampleComponent';
+
+const App: React.FC = () => {
+    return (
+        <div>
+            <h1>congnguyen</h1>
+            <ExampleComponent />
+        </div>
+    );
+};
+
+export default App;
